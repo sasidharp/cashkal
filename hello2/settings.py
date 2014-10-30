e42bd7b50635d1b5904ea604926ca0551a90ea96
sasidharp <sasidharp@gmail.com>
2014-10-31 01:20:52 +0530

    hello push

--- hello2/settings.py
+++ hello2/settings.py
@@ -7,12 +7,8 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -51,48 +47,33 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'hello2.urls'
-
 WSGI_APPLICATION = 'hello2.wsgi.application'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 TEMPLATE_DIRS=(
     os.path.join(os.path.dirname(BASE_DIR),"static","templates")
 )
 
 
-if DEBUG:
-    # STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
-    # STATICFILES_DIRS=(
-    #                   os.path.join(os.path.dirname(BASE_DIR),"static","static"),
-    #                   )
-    STATIC_ROOT='C:\\Users\\sapurana.FAREAST\\work\\staticout'
-    STATICFILES_DIRS= ( 'C:\\Users\\sapurana.FAREAST\\work\\staticsample',)
 
