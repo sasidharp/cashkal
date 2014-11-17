65efe2cbb4c2616acb6d236ece2086c44c64e9a4
sasidhar puranam <sasidharp@gmail.com>
2014-11-18 00:20:08 +0530

    redeploy

--- hello2/settings.py
+++ hello2/settings.py
@@ -59,7 +59,7 @@ USE_L10N = True
 USE_TZ = True
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
- DATABASES = {
+DATABASES = {
      "default": dj_database_url.config(default='postgres://localhost'),
 }
 #DATABASES = {
